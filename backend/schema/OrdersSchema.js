5943957265d08b76ff23010a83acd4bb0d0b1883
javedhussain17092004 <jh880232@gmail.com>
2025-09-03 13:41:28 +0530

    Initial commit

---
+++ backend/schema/OrdersSchema.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+
+const OrdersSchema=new mongoose.Schema({  
+    name: String,
+    qty: Number,
+    price: Number,
+    mode:String,
+  
+})
+
+
+module.exports={OrdersSchema};
